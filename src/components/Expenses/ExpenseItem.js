0b91bcae1a4b8032c4770c0052b521d56efdba25
P-Jeremy <jemyplu@gmail.com>
2022-02-04 14:11:11 +0100

    :sparkles: feat(testing): add card ui component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from '../ui/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.scss';
 
 const ExpenseItem = (props) => {
 
   return (
-    <div className='card expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
