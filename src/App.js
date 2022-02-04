81cd25c0337f367eb6e1ae7097cecab9815a54ee
P-Jeremy <jemyplu@gmail.com>
2022-02-04 09:24:22 +0100

    :sparkles: feat(ExpanseDate): add component to display whole expense item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import ExpenseDate from './components/Expenses/ExpenseDate';
+import ExpenseItem from './components/Expenses/ExpenseItem';
 
 function App() {
   const date = new Date('2022-01-01')
+  const amount = 50;
+  const title = 'Fake Item';
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseDate date={date}/>
+      <ExpenseItem title={title} amount={amount} date={date}/>
     </div>
   );
 }
---
+++ src/tests/integration/Expenses/ExpenseItem.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ExpenseItem from '../../../components/Expenses/ExpenseItem';
+
+describe('INTEGRATION | Component | ExpenseItem', function () {
+  describe('When given an item', function () {
+    it('should render', function () {
+      // given
+      const date = new Date('2022-01-01');
+      const title = 'Item test';
+      const amout = 42;
+
+      // when
+      render(<ExpenseItem date={date} title={title} amount={amout} />);
+
+      // then
+      expect(screen.getByText('01')).toBeInTheDocument();
+      expect(screen.getByText('janvier')).toBeInTheDocument();
+      expect(screen.getByText('2022')).toBeInTheDocument();
+      expect(screen.getByText('Item test')).toBeInTheDocument();
+    });
+  });
+});
